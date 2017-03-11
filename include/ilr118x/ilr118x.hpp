e4cf032aa99f5b4bbe4823cba4405d3f804022c4
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2017-03-11 20:21:34 +0900

    big changes - add sleep before restart - ros integration

--- include/ilr118x/ilr118x.hpp
+++ include/ilr118x/ilr118x.hpp
@@ -1,34 +1,54 @@
 #ifndef ILR118X_ILR118X
 #define ILR118X_ILR118X
 
-#include <iostream>
-#include <stdexcept>
 #include <string>
 
 #include <boost/asio/buffer.hpp>
 #include <boost/asio/deadline_timer.hpp>
+#include <boost/asio/error.hpp>
 #include <boost/asio/io_service.hpp>
 #include <boost/asio/read_until.hpp>
 #include <boost/asio/serial_port.hpp>
 #include <boost/asio/streambuf.hpp>
 #include <boost/asio/write.hpp>
 #include <boost/bind.hpp>
-#include <boost/date_time/posix_time/posix_time_types.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/system/error_code.hpp>
 #include <boost/system/system_error.hpp>
 
 #include <ros/console.h>
+#include <ros/duration.h>
+#include <ros/names.h>
+#include <ros/node_handle.h>
+#include <ros/param.h>
+#include <ros/publisher.h>
+#include <std_msgs/Float64.h>
 
 namespace ilr118x {
 
+namespace ba = boost::asio;
+namespace bs = boost::system;
+namespace rn = ros::names;
+namespace rp = ros::param;
+
 class ILR118x {
 public:
-  ILR118x(boost::asio::io_service &service, const std::string &device)
-      : serial_(service), timer_(service), device_(device) {}
+  ILR118x(ba::io_service &service, ros::NodeHandle &handle, const std::string &ns = "~")
+      : serial_(service), timer_(service) {
+    // load parameters
+    device_ = rp::param< std::string >(rn::append(ns, "device"), "/dev/ttyUSB0");
+    timeout_ = ros::Duration(rp::param(rn::append(ns, "timeout"), 5.)).toBoost();
+
+    // create a ros topic
+    publisher_ = handle.advertise< std_msgs::Float64 >("distance", 1);
+
+    // start the operation
+    start();
+  }
 
   virtual ~ILR118x() {}
 
+private:
   void start() {
     try {
       // close the device if already opened
@@ -40,24 +60,20 @@ public:
       serial_.open(device_);
 
       // set options
-      typedef boost::asio::serial_port Serial;
-      serial_.set_option(Serial::baud_rate(9600));
-      serial_.set_option(Serial::flow_control(Serial::flow_control::none));
-      serial_.set_option(Serial::parity(Serial::parity::none));
-      serial_.set_option(Serial::stop_bits(Serial::stop_bits::one));
-      serial_.set_option(Serial::character_size(8));
-    } catch (const boost::system::system_error &error) {
+      serial_.set_option(ba::serial_port::baud_rate(9600));
+      serial_.set_option(ba::serial_port::flow_control(ba::serial_port::flow_control::none));
+      serial_.set_option(ba::serial_port::parity(ba::serial_port::parity::none));
+      serial_.set_option(ba::serial_port::stop_bits(ba::serial_port::stop_bits::one));
+      serial_.set_option(ba::serial_port::character_size(8));
+    } catch (const bs::system_error &error) {
       // restart on error
       ROS_ERROR_STREAM("On starting operation: " << error.what());
-      start();
+      restart();
       return;
     }
 
     // clear the read buffer
-    read_buffer_.consume(read_buffer_.size());
-
-    // enable timeout
-    enableTimer();
+    buffer_.consume(buffer_.size());
 
     // start writing the first request
     startWriteFlushRequest();
@@ -66,15 +82,18 @@ public:
 private:
   void startWriteFlushRequest() {
     // write an invalied command to flush the buffer on the device
-    startTimer();
-    boost::asio::async_write(serial_, boost::asio::buffer("XX\r", 3),
-                             boost::bind(&ILR118x::handleWriteFlushRequest, this, _1));
+    timer_.expires_from_now(timeout_);
+    timer_.async_wait(boost::bind(&ILR118x::handleTimeout, this, _1));
+    ba::async_write(serial_, ba::buffer("XX\r", 3),
+                    boost::bind(&ILR118x::handleWriteFlushRequest, this, _1));
   }
 
-  void handleWriteFlushRequest(const boost::system::error_code &error) {
+  void handleWriteFlushRequest(const bs::error_code &error) {
+    timer_.cancel();
+
     if (error) {
       ROS_ERROR_STREAM("On writing flush request: " << error.message());
-      start();
+      restart();
       return;
     }
 
@@ -83,41 +102,47 @@ private:
 
   void startReadFlushResponse() {
     // receive a response indicating an invalid command
-    startTimer();
-    boost::asio::async_read_until(serial_, read_buffer_, "E61\r\n",
-                                  boost::bind(&ILR118x::handleReadFlushResponse, this, _1, _2));
+    timer_.expires_from_now(timeout_);
+    timer_.async_wait(boost::bind(&ILR118x::handleTimeout, this, _1));
+    ba::async_read_until(serial_, buffer_, "E61\r\n",
+                         boost::bind(&ILR118x::handleReadFlushResponse, this, _1, _2));
   }
 
-  void handleReadFlushResponse(const boost::system::error_code &error, const std::size_t bytes) {
+  void handleReadFlushResponse(const bs::error_code &error, const std::size_t bytes) {
+    timer_.cancel();
+
     if (error) {
-      std::cerr << "On reading flush response: " << error.message() << std::endl;
-      start();
+      ROS_ERROR_STREAM("On reading flush response: " << error.message());
+      restart();
       return;
     }
 
     /*
     {
-      const char *const data(boost::asio::buffer_cast< const char * >(read_buffer_.data()));
+      const char *const data(ba::buffer_cast< const char * >(buffer_.data()));
       const std::size_t size(bytes - 2);
       ROS_INFO_STREAM(" > " << std::string(data, size));
     }
     */
 
-    read_buffer_.consume(bytes);
+    buffer_.consume(bytes);
 
     startWriteResetRequest();
   }
 
   void startWriteResetRequest() {
-    startTimer();
-    boost::asio::async_write(serial_, boost::asio::buffer("PR\r", 3),
-                             boost::bind(&ILR118x::handleWriteResetRequest, this, _1));
+    timer_.expires_from_now(timeout_);
+    timer_.async_wait(boost::bind(&ILR118x::handleTimeout, this, _1));
+    ba::async_write(serial_, ba::buffer("PR\r", 3),
+                    boost::bind(&ILR118x::handleWriteResetRequest, this, _1));
   }
 
-  void handleWriteResetRequest(const boost::system::error_code &error) {
+  void handleWriteResetRequest(const bs::error_code &error) {
+    timer_.cancel();
+
     if (error) {
       ROS_ERROR_STREAM("On writing reset request: " << error.message());
-      start();
+      restart();
       return;
     }
 
@@ -125,27 +150,29 @@ private:
   }
 
   void startReadResetResponse(const std::size_t count = 16) {
-    startTimer();
-    boost::asio::async_read_until(
-        serial_, read_buffer_, "\r\n",
-        boost::bind(&ILR118x::handleReadResetResponse, this, count, _1, _2));
+    timer_.expires_from_now(timeout_);
+    timer_.async_wait(boost::bind(&ILR118x::handleTimeout, this, _1));
+    ba::async_read_until(serial_, buffer_, "\r\n",
+                         boost::bind(&ILR118x::handleReadResetResponse, this, count, _1, _2));
   }
 
-  void handleReadResetResponse(const std::size_t count, const boost::system::error_code &error,
+  void handleReadResetResponse(const std::size_t count, const bs::error_code &error,
                                const std::size_t bytes) {
+    timer_.cancel();
+
     if (error) {
-      std::cerr << "On reading reset response: " << error.message() << std::endl;
-      start();
+      ROS_ERROR_STREAM("On reading reset response: " << error.message());
+      restart();
       return;
     }
 
     {
-      const char *const data(boost::asio::buffer_cast< const char * >(read_buffer_.data()));
+      const char *const data(ba::buffer_cast< const char * >(buffer_.data()));
       const std::size_t size(bytes - 2);
       ROS_INFO_STREAM(" > " << std::string(data, size));
     }
 
-    read_buffer_.consume(bytes);
+    buffer_.consume(bytes);
 
     const std::size_t next_count(count - 1);
     if (next_count > 0) {
@@ -156,15 +183,18 @@ private:
   }
 
   void startWriteStreamRequest() {
-    startTimer();
-    boost::asio::async_write(serial_, boost::asio::buffer("DX\r", 3),
-                             boost::bind(&ILR118x::handleWriteStreamRequest, this, _1));
+    timer_.expires_from_now(timeout_);
+    timer_.async_wait(boost::bind(&ILR118x::handleTimeout, this, _1));
+    ba::async_write(serial_, ba::buffer("DX\r", 3),
+                    boost::bind(&ILR118x::handleWriteStreamRequest, this, _1));
   }
 
-  void handleWriteStreamRequest(const boost::system::error_code &error) {
+  void handleWriteStreamRequest(const bs::error_code &error) {
+    timer_.cancel();
+
     if (error) {
-      std::cerr << "On writing stream request: " << error.message() << std::endl;
-      start();
+      ROS_ERROR_STREAM("On writing stream request: " << error.message());
+      restart();
       return;
     }
 
@@ -172,29 +202,33 @@ private:
   }
 
   void startReadStreamResponse() {
-    startTimer();
-    boost::asio::async_read_until(serial_, read_buffer_, "\r\n",
-                                  boost::bind(&ILR118x::handleReadStreamResponse, this, _1, _2));
+    timer_.expires_from_now(timeout_);
+    timer_.async_wait(boost::bind(&ILR118x::handleTimeout, this, _1));
+    ba::async_read_until(serial_, buffer_, "\r\n",
+                         boost::bind(&ILR118x::handleReadStreamResponse, this, _1, _2));
   }
 
-  void handleReadStreamResponse(const boost::system::error_code &error, const std::size_t bytes) {
+  void handleReadStreamResponse(const bs::error_code &error, const std::size_t bytes) {
+    timer_.cancel();
+
     if (error) {
-      std::cerr << "On reading stream response: " << error.message() << std::endl;
-      start();
+      ROS_ERROR_STREAM("On reading stream response: " << error.message());
+      restart();
       return;
     }
 
     // parse the response
     {
       // access the response string except the delimiter
-      const char *const data(boost::asio::buffer_cast< const char * >(read_buffer_.data()));
+      const char *const data(ba::buffer_cast< const char * >(buffer_.data()));
       const std::size_t size(bytes - 2);
       // print the response string for debug
       ROS_INFO_STREAM(" > " << std::string(data, size));
       // convert the reponse string to a number
-      double value;
-      if (boost::conversion::try_lexical_convert(data, size, value)) {
-        // TODO: publish the value
+      std_msgs::Float64 distance;
+      if (boost::conversion::try_lexical_convert(data, size, distance.data)) {
+        // publish the distance value
+        publisher_.publish(distance);
       } else {
         ROS_WARN_STREAM("On parsing stream response: "
                         << "bad conversion: " << std::string(data, size));
@@ -202,30 +236,55 @@ private:
     }
 
     // remove the parsed response from the buffer
-    read_buffer_.consume(bytes);
+    buffer_.consume(bytes);
 
     // receive the next response
     startReadStreamResponse();
   }
 
-  void enableTimer() { timer_.async_wait(boost::bind(&ILR118x::handleTimer, this)); }
-
-  void startTimer() { timer_.expires_from_now(boost::posix_time::seconds(10)); }
-
-  void handleTimer() {
-    if (timer_.expires_at() <= boost::asio::deadline_timer::traits_type::now()) {
-      serial_.close();
-      timer_.expires_at(boost::posix_time::pos_infin);
+  void handleTimeout(const bs::error_code &error) {
+    if (error == ba::error::operation_aborted) {
+      return;
     }
-    enableTimer();
+
+    if (error) {
+      ROS_ERROR_STREAM("On waiting timeout: " << error.message());
+      return;
+    }
+
+    serial_.cancel();
+  }
+
+  void restart() {
+    timer_.expires_from_now(timeout_);
+    timer_.async_wait(boost::bind(&ILR118x::handleRestart, this, _1));
+  }
+
+  void handleRestart(const bs::error_code &error) {
+    if (error == ba::error::operation_aborted) {
+      return;
+    }
+
+    if (error) {
+      ROS_ERROR_STREAM("On waiting restart: " << error.message());
+      return;
+    }
+
+    start();
   }
 
 private:
-  boost::asio::serial_port serial_;
-  boost::asio::deadline_timer timer_;
-  const std::string device_;
+  // parameters
+  std::string device_;
+  ba::deadline_timer::duration_type timeout_;
 
-  boost::asio::streambuf read_buffer_;
+  // sensor drivers
+  ba::serial_port serial_;
+  ba::streambuf buffer_;
+  ba::deadline_timer timer_;
+
+  // ros publisher
+  ros::Publisher publisher_;
 };
 }
 
--- src/ilr118x_node.cpp
+++ src/ilr118x_node.cpp
@@ -1,13 +1,26 @@
+#include <csignal>
+
 #include <boost/asio/io_service.hpp>
+
+#include <ros/init.h>
+#include <ros/node_handle.h>
 
 #include <ilr118x/ilr118x.hpp>
 
+boost::asio::io_service service;
+
+void handleSigint(int) {
+  service.stop();
+  std::signal(SIGINT, handleSigint);
+}
+
 int main(int argc, char *argv[]) {
 
-  boost::asio::io_service service;
+  ros::init(argc, argv, "ilr118x_node", ros::init_options::NoSigintHandler);
+  std::signal(SIGINT, handleSigint);
 
-  ilr118x::ILR118x device(service, "/dev/ttyUSB0");
-  device.start();
+  ros::NodeHandle handle;
+  ilr118x::ILR118x device(service, handle);
 
   service.run();
 
